1bbac5bcc3fc105c592161df4b82107418e271d3
Nameless <dev@name1ess.ga>
2024-03-22 22:25:49 +0000

    Update NoMoneyLivery.lua
    Removed livery change
    Added vehicle model match
    Added no max tune livery
    Fixed bugs

--- NoMoneyLivery.lua
+++ NoMoneyLivery.lua
@@ -1,25 +1,44 @@
+util.require_natives("3095a")
+
 local VMT_LIVERY_MOD = 48
 
 local update_delay = 100 -- ms
-local livery_ids = { -- [new_livery_id] = old_livery_id;
+
+local livery_ids = { -- [vehicle_name] = livery_id;
 	39; -- money livery
-	--11; -- raiju livery
+	--["raiju"] = 11; -- raiju livery
 }
 
-local default_new_livery_id = -1
+local no_max_vehicle_livery = false
+local new_livery_id = -1
 
 local function update()
-	local vehicle = entities.get_user_vehicle_as_pointer()
-	if vehicle == 0 then
+	local vehicle = entities.get_user_vehicle_as_handle()
+	if vehicle == INVALID_GUID then
 		return
 	end
 	
 	local livery_id = entities.get_upgrade_value(vehicle, VMT_LIVERY_MOD)
-	if not (livery_id in livery_ids) then
+	
+	local found = livery_ids:contains(livery_id)
+	if not found then
 		return
 	end
 	
-	local new_livery_id = livery_ids:contains(livery_id) ?? default_new_livery_id
+	if no_max_vehicle_livery then
+		local max_livery_id = entities.get_upgrade_max_value(vehicle, VMT_LIVERY_MOD)
+	
+		if livery_id == max_livery_id then
+			entities.set_upgrade_value(vehicle, VMT_LIVERY_MOD, new_livery_id)
+		end
+		
+		return
+	end
+
+	-- vehicle check
+	if found and string.isalpha(found) and not VEHICLE.IS_VEHICLE_MODEL(vehicle, util.joaat(found)) then 
+		return
+	end
 	
 	entities.set_upgrade_value(vehicle, VMT_LIVERY_MOD, new_livery_id)
 end
